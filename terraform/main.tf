1736d0d0064867c2c8196fae2bb5497ecda2298b
bannmoore <moore.brittanyann@gmail.com>
2018-07-24 21:48:45 -0400

    Add basic terraform instance definition.

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "bam" {
+  ami                    = "${var.ami}"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+            #!/bin/bash
+            echo "Hello, World" > index.html
+            nohup busybox httpd -f -p "${var.server_port}" &
+            EOF
+
+  tags {
+    Name = "bam-app"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "bam-app-instance"
+
+  ingress {
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default     = 8080
+}
+
+variable "ami" {
+  description = "The AMI used by the ec2 instance"
+  default     = "ami-2757f631"
+}
+
+output "public_ip" {
+  value = "${aws_instance.bam.public_ip}"
+}
